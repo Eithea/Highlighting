02750a577f3901fc2a5bcee9835370b10e64b4ae
Eithea <oguraff94@gmail.com>
2022-04-14 03:10:40 +0900

    와이어프레임대로 Cover div scss 작성

--- src/pages/ArchiveViewer.jsx
+++ src/pages/ArchiveViewer.jsx
@@ -1,28 +1,35 @@
+import "./ArchiveViewer.scss"
+
 import Header from "../components/header/Header";
 
 function ArchiveViewer() {
     return (
-        <div>
+        <>
             <Header/>
-            <div classname = 'ArchiveViewer'>
-                <div classname = 'Viewers'>
-                    <div classname = 'Broadcast'>
-                        <div classname = 'Video'>
-                            
+            <div class = 'Cover-ArchiveViewer'>
+                <div class = 'Cover-Viewers'>
+                    <div class = 'Cover-Broadcast'>
+                        <div class = 'Cover-Video'>
+                            <div class = 'Comp-VideoPlayer'>(비디오 플레이어)</div>
+                            <div class = 'Comp-VideoController'>(비디오 제어)</div>
                         </div>
-                        <div classname = 'Chat'>
-
+                        <div class = 'Cover-Chat'>
+                        <div class = 'Comp-ChatViewer'>(채팅 뷰어)</div>
+                        <div class = 'Comp-ChatAnalyzer'>(채팅 분석)</div>
                         </div>
                     </div>
-                    <div classname = 'Data'>
-                        
+                    <div class = 'Cover-Data'>
+                        <div class = 'Comp-DataIndex'>(데이터 인덱스)</div>
+                        <div class = 'Comp-DataChart'>(데이터 차트)</div>
                     </div>
                 </div>
-                <div classname = 'Sidebar'>
-
+                <div class = 'Cover-Sidebar'>
+                    <div class = 'Comp-ViewChanger'>(사이드바 뷰 체인저)</div>
+                    <div class = 'Comp-ClipList'>(클립 리스트)</div>
+                    <div class = 'Comp-ArchiveList'>(아카이브 리스트)</div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
