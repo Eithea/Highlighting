005c043a7861d54f3af948b875989ced1a3a740e
Eithea <oguraff94@gmail.com>
2022-04-15 01:14:16 +0900

    handler function 분리

--- server/server.go
+++ server/server.go
@@ -3,18 +3,27 @@ package main
 import "fmt"
 import "net/http"
 
+
+func handler_home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "home")
+}
+
+func handler_path1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "1111")
+}
+
+func handler_path2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "2222")
+}
+
+
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w, "Main")
-	})
+	http.HandleFunc("/", handler_home)
 
-	http.HandleFunc("/path1", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w, "path1")
-	})
+	http.HandleFunc("/path1", handler_path1)
 
-	http.HandleFunc("/path2", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w, "path2")
-	})
+	http.HandleFunc("/path2", handler_path2)
 
 	http.ListenAndServe(":3000", nil)
 }
