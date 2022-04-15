ac23b5a7d4a4630cb323bf43790907f2567c9287
Eithea <oguraff94@gmail.com>
2022-04-16 02:37:53 +0900

    Go mod 연습

---
+++ server/go.mod
@@ -0,0 +1,7 @@
+module main
+
+go 1.18
+
+replace src => ./src
+
+require src v0.0.0-00010101000000-000000000000
--- server/server.go
+++ server/server.go
@@ -1,36 +1,42 @@
 package main
 
-import "fmt"
-import "net/http"
+// import "fmt"
+// import "net/http"
 
 
-func handler_home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "home")
-}
+// func handler_home(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "home")
+// }
 
-func handler_GET_static(w http.ResponseWriter, r *http.Request) {
-	// do some static logic
-	fmt.Fprint(w, "result of GET")
-}
+// func handler_GET_static(w http.ResponseWriter, r *http.Request) {
+// 	// do some static logic
+// 	fmt.Fprint(w, "result of GET")
+// }
 
-func handler_GET_dynamic(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query().Get("q")
-	if q == "" {
-		q = "nothing"
-	}
-	// do some dynamic logic
-	fmt.Fprint(w, "result of GET\n")
-	fmt.Fprint(w, "q is " + q)
-}
+// func handler_GET_dynamic(w http.ResponseWriter, r *http.Request) {
+// 	q := r.URL.Query().Get("q")
+// 	if q == "" {
+// 		q = "nothing"
+// 	}
+// 	// do some dynamic logic
+// 	fmt.Fprint(w, "result of GET\n")
+// 	fmt.Fprint(w, "q is " + q)
+// }
 
 
+
+// func main() {
+// 	http.HandleFunc("/", handler_home)
+
+// 	http.HandleFunc("/GETtest_static", handler_GET_static)
+
+// 	http.HandleFunc("/GETtest_dynamic", handler_GET_dynamic)
+
+// 	http.ListenAndServe(":8080", nil)
+// }
+
+import "src/hello"
 
 func main() {
-	http.HandleFunc("/", handler_home)
-
-	http.HandleFunc("/GETtest_static", handler_GET_static)
-
-	http.HandleFunc("/GETtest_dynamic", handler_GET_dynamic)
-
-	http.ListenAndServe(":8080", nil)
+	hello.Hello()
 }
